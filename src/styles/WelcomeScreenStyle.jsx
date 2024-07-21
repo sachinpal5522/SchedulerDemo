1348633e28d5cb8a097be3ac73664fdfd8b0509d
sachinpal5522 <sachinpal552@gmail.com>
2024-07-21 17:57:09 +0530

    css file for welcome screen

---
+++ src/styles/WelcomeScreenStyle.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  font-weight: 500;
+`;
